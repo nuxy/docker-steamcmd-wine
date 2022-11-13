dc7a29c7ab6c9c43b5f85a595adae398a2276638
Marc S. Brooks <marc@mbrooks.info>
2022-11-12 21:17:37 -0800

    Removed WINE init, DBUS workaround

--- launch.sh
+++ launch.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-# First start generates .wine sources which result
-# in a broken DBUS connection (winsock failure)
+# Start services
 service game-server start
-
-# .. so we restart the service and game server.
-service dbus restart
-service game-server restart
 
 # Keep-alive
 sleep infinity
